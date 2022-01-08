bcb57574e4e48d008ce9705e19dd80bd1aca4e38
YuriiKnysh <uriy.knish@gmail.com>
2022-01-08 18:48:51 +0400

    Workingwithcollections

---
+++ Collections/Program.cs
@@ -0,0 +1,122 @@
+﻿List<string> people = new List<string>();
+
+List<Person> people1 = new List<Person>()
+{
+    new Person("Tom"),
+    new Person("Bob"),
+    new Person("Sam")
+};
+var people2 = new List<string>() { "Tom", "Bob", "Sam" };
+
+string firstPerson = people2[0]; // получаем первый элемент
+Console.WriteLine(firstPerson); // Tom
+people2[0] = "Mike";     // изменяем первый элемент
+Console.WriteLine(people2[0]); // Mike
+//Перебор списка
+var people3 = new List<string>() { "Tom", "Bob", "Sam" };
+
+foreach (var person in people3)
+{
+    Console.WriteLine(person);
+}
+
+List<string> people4 = new List<string>() { "Tom" };
+
+people4.Add("Bob"); // добавление элемента
+                   // people = { "Tom", "Bob" };
+
+people4.AddRange(new[] { "Sam", "Alice" });   // добавляем массив
+                                             // people = { "Tom", "Bob", "Sam", "Alice" };
+                                             // также можно было бы добавить другой список
+                                             // people.AddRange(new List<string>(){ "Sam", "Alice" });
+
+people4.Insert(0, "Eugene"); // вставляем на первое место
+                            // people = { "Eugene", "Tom", "Bob", "Sam", "Alice" };
+
+people4.InsertRange(1, new string[] { "Mike", "Kate" }); // вставляем массив с индекса 1
+// people = { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam", "Alice" };
+
+// также можно было бы добавить другой список
+// people.InsertRange(1, new List<string>(){ "Mike", "Kate" });
+//</ string ></ string >
+
+//Удаление из списка
+var people5 = new List<string>() { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam", "Tom", "Alice" };
+
+people5.RemoveAt(1); //  удаляем второй элемент
+                    // people = { "Eugene", "Kate", "Tom", "Bob", "Sam", "Tom", "Alice" };
+
+people5.Remove("Tom"); //  удаляем элемент "Tom"
+                      // people = { "Eugene", "Kate", "Bob", "Sam", "Tom", "Alice" };
+
+// удаляем из списка все элементы, длина строки которых равна 3
+people5.RemoveAll(person => person.Length == 3);
+// people = { "Eugene", "Kate", "Alice" };
+
+// удаляем из списка 2 элемента начиная с индекса 1
+people5.RemoveRange(1, 2);
+// people = { "Eugene"};
+
+// полностью очищаем список
+people5.Clear();
+// people = {  };
+
+
+
+var people6 = new List<string>() { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam" };
+
+var containsBob = people6.Contains("Bob");     //  true
+var containsBill = people6.Contains("Bill");    // false
+
+// проверяем, есть ли в списке строки с длиной 3 символа
+var existsLength3 = people6.Exists(p => p.Length == 3);  // true
+
+// проверяем, есть ли в списке строки с длиной 7 символов
+var existsLength7 = people6.Exists(p => p.Length == 7);  // false
+
+// получаем первый элемент с длиной в 3 символа
+var firstWithLength3 = people6.Find(p => p.Length == 3); // Tom
+
+// получаем последний элемент с длиной в 3 символа
+var lastWithLength3 = people6.FindLast(p => p.Length == 3);  // Sam
+
+// получаем все элементы с длиной в 3 символа в виде списка
+List<string> peopleWithLength3 = people6.FindAll(p => p.Length == 3);
+// peopleWithLength3 { "Tom", "Bob", "Sam"}
+
+List<string> people7 = new List<string>() { "Eugene", "Tom", "Mike", "Sam", "Bob" };
+
+// получаем диапазон со второго по четвертый элемент
+var range = people7.GetRange(1, 3);
+// range = { "Tom", "Mike", "Sam"};
+
+// копируем в массив первые три элемента
+string[] partOfPeople = new string[3];
+people7.CopyTo(0, partOfPeople, 0, 3);
+// partOfPeople = { "Eugene", "Tom", "Mike"};
+
+
+var people8 = new List<string>() { "Eugene", "Tom", "Mike", "Sam", "Bob" };
+
+// переворачиваем весь список
+people8 = new List<string>() { "Eugene", "Tom", "Mike", "Sam", "Bob" };
+// переворачиваем часть только 3 элемента с индекса 1
+people8.Reverse(1, 3);
+// people2 = { "Eugene","Sam", "Mike", "Tom", "Bob" };
+
+List<string> people9 = new List<string>() { "Eugene", "Tom", "Mike", "Sam", "Bob" };
+
+// получаем диапазон со второго по четвертый элемент
+var range1 = people9.GetRange(1, 3);
+// range = { "Tom", "Mike", "Sam"};
+
+// копируем в массив первые три элемента
+string[] partOfPeople1 = new string[3];
+people9.CopyTo(0, partOfPeople1, 0, 3);
+// partOfPeople = { "Eugene", "Tom", "Mike"};
+
+class Person
+{
+    public string Name { get; }
+    public Person(string name) => Name = name;
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,54 @@
+﻿
+var employees = new List<string> { "Tom", "Sam", "Bob" };
+
+LinkedList<string> people = new LinkedList<string>(employees);
+foreach (string person in people)
+{
+    Console.WriteLine(person);
+}
+
+LinkedList<string> people1 = new LinkedList<string>(employees);
+foreach (string person in people1)
+{
+    Console.WriteLine(person);
+}
+
+var employees1 = new List<string> { "Tom", "Sam", "Bob" };
+
+LinkedList<string> people2 = new LinkedList<string>(employees1);
+Console.WriteLine(people2.Count);            // 3
+Console.WriteLine(people2.First?.Value);    // Tom
+Console.WriteLine(people2.Last?.Value);    // Bob
+
+
+LinkedList<string> people3 = new LinkedList<string>(new[] { "Tom", "Sam", "Bob" });
+
+// от начала до конца списка
+var currentNode = people3.First;
+while (currentNode != null)
+{
+    Console.WriteLine(currentNode.Value);
+    currentNode = currentNode.Next;
+}
+
+// с конца до начала списка
+currentNode = people3.Last;
+while (currentNode != null)
+{
+    Console.WriteLine(currentNode.Value);
+    currentNode = currentNode.Previous;
+}
+
+
+var people4 = new LinkedList<string>();
+people4.AddLast("Tom"); // вставляем узел со значением Tom на последнее место
+                       //так как в списке нет узлов, то последнее будет также и первым
+people4.AddFirst("Bob"); // вставляем узел со значением Bob на первое место
+
+// вставляем после первого узла новый узел со значением Mike
+if (people4.First != null) people4.AddAfter(people4.First, "Mike");
+
+// теперь у нас список имеет следующую последовательность: Bob Mike Tom
+foreach (var person in people4) Console.WriteLine(person);
+
+
