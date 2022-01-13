66160b6fa36defbb5b94953c5522b18b178702ff
YuriiKnysh <uriy.knish@gmail.com>
2022-01-13 16:25:13 +0400

    WorkingwithDates

---
+++ DateOnlyandTimeOnly/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Globalization;
+
+DateOnly julianDate = new DateOnly(2022, 1, 6, new JulianCalendar());
+Console.WriteLine(julianDate); // 19.01.2022
+//DateOnly();
+///DateOnly(int year, int month, int day);
+//DateOnly(int year, int month, int day);
+//DateOnly(int year, int month, int day, System.Globalization.Calendar calendar);
+
+DateOnly now = new DateOnly(2022, 1, 6);
+Console.WriteLine(now.Day);         // 6
+Console.WriteLine(now.DayNumber);   // 738160
+Console.WriteLine(now.DayOfWeek);   // Thursday
+Console.WriteLine(now.DayOfYear);   // 6
+Console.WriteLine(now.Month);       // 1
+Console.WriteLine(now.Year);        // 2022
+
+DateOnly now1 = DateOnly.Parse("06.01.2022");
+Console.WriteLine(now1); // 06.01.2022
+now1 = now1.AddDays(1);   // 07.01.2022
+now1 = now1.AddMonths(4); // 07.04.2022
+now1 = now1.AddYears(-1); // 07.05.2021
+Console.WriteLine(now1.ToShortDateString());  // 07.05.2021
+Console.WriteLine(now1.ToLongDateString());   // 7 мая 2021 г.
+
+TimeOnly time1 = new TimeOnly(4, 30);
+Console.WriteLine(time1);   // 4: 30
+
+TimeOnly time2 = new TimeOnly(14, 23, 30);
+Console.WriteLine(time2);   // 14: 23
+
+TimeOnly time = new TimeOnly(14, 23, 30);
+Console.WriteLine(time.Hour);       // 14
+Console.WriteLine(time.Minute);     // 23
+Console.WriteLine(time.Second);     // 30
+
+TimeOnly time3 = TimeOnly.Parse("06:33:22");
+Console.WriteLine(time);        // 6:33
+time3 = time3.AddHours(1);        // 7:33
+time3 = time3.AddMinutes(-23);   // 7:10
+
+Console.WriteLine(time3.ToShortTimeString());  // 7:10
+Console.WriteLine(time3.ToLongTimeString());   // 7:10:22
---
+++ Workingwithdatesandtimes/Program.cs
@@ -0,0 +1,45 @@
+﻿DateTime date1 = new DateTime(2022, 1, 13); // год - месяц - день
+Console.WriteLine(date1); // 13.01.2022 0:00:00
+
+Console.WriteLine(DateTime.Now);
+Console.WriteLine(DateTime.UtcNow);
+Console.WriteLine(DateTime.Today);
+
+DateTime date2 = new DateTime(2015, 7, 20, 18, 30, 25); // 20.07.2015 18:30:25
+Console.WriteLine(date2.AddHours(3)); // 20.07.2015 21:30:25
+
+DateTime date3 = new DateTime(2015, 7, 20, 18, 30, 25); // 20.07.2015 18:30:25
+DateTime date4 = new DateTime(2015, 7, 20, 15, 30, 25); // 20.07.2015 15:30:25
+Console.WriteLine(date3.Subtract(date4)); // 03:00:00
+DateTime date = new DateTime(2015, 7, 20, 18, 30, 25);
+
+
+Console.WriteLine(date.ToLocalTime()); // 20.07.2015 21:30:25
+Console.WriteLine(date.ToUniversalTime()); // 20.07.2015 15:30:25
+Console.WriteLine(date.ToLongDateString()); // 20 июля 2015 г.
+Console.WriteLine(date.ToShortDateString()); // 20.07.2015
+Console.WriteLine(date.ToLongTimeString()); // 18:30:25
+Console.WriteLine(date.ToShortTimeString()); // 18:30
+
+DateTime now = DateTime.Now;
+
+Console.WriteLine($"D: {now.ToString("D")}");
+Console.WriteLine($"d: {now.ToString("d")}");
+Console.WriteLine($"F: {now.ToString("F")}");
+Console.WriteLine($"f: {now:f}");
+Console.WriteLine($"G: {now:G}");
+Console.WriteLine($"g: {now:g}");
+Console.WriteLine($"M: {now:M}");
+Console.WriteLine($"O: {now:O}");
+Console.WriteLine($"o: {now:o}");
+Console.WriteLine($"R: {now:R}");
+Console.WriteLine($"s: {now:s}");
+Console.WriteLine($"T: {now:T}");
+Console.WriteLine($"t: {now:t}");
+Console.WriteLine($"U: {now:U}");
+Console.WriteLine($"u: {now:u}");
+Console.WriteLine($"Y: {now:Y}");
+
+DateTime now1 = DateTime.Now;
+Console.WriteLine(now1.ToString("hh:mm:ss"));
+Console.WriteLine(now1.ToString("dd.MM.yyyy"));
