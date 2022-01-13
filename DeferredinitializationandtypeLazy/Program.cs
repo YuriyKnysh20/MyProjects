50338a4c86e33cd1e72cf05e99c831baaecc3a51
YuriiKnysh <uriy.knish@gmail.com>
2022-01-13 17:50:39 +0400

    LazyType

---
+++ DeferredinitializationandtypeLazy/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Reader reader = new Reader();
+reader.ReadEbook();
+reader.ReadBook();
+class Reader
+{
+    Lazy<Library> library = new Lazy<Library>();
+    public void ReadBook()
+    {
+        library.Value.GetBook();
+        Console.WriteLine("Читаем бумажную книгу");
+    }
+
+    public void ReadEbook()
+    {
+        Console.WriteLine("Читаем книгу на компьютере");
+    }
+}
+
+class Library
+{
+    private string[] books = new string[99];
+
+    public void GetBook()
+    {
+        Console.WriteLine("Выдаем книгу читателю");
+    }
+}
