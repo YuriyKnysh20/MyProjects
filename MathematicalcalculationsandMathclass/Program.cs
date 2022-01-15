369b53d0f09f54f29d6b839df8c5ea488afa83fe
YuriiKnysh <uriy.knish@gmail.com>
2022-01-15 13:02:41 +0400

    Workingwithdiffentmath

--- MathematicalcalculationsandMathclass/Program.cs
+++ MathematicalcalculationsandMathclass/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double result  = Math.Abs(-12.4); // 12.4
+Console.WriteLine(result);
+double result1 = Math.Acos(1); // 0
+Console.WriteLine(result1);
+double result2 = Math.Asin(1);
+Console.WriteLine(result2);
+double result3 =Math.Atan(1);
+Console.WriteLine(result3);
+double result4 = Math.BigMul(100, 200);
+Console.WriteLine(result4);
+double result5 = Math.Ceiling(2.34);
+Console.WriteLine(result5);
+double result6 = Math.Cos(1);
+Console.WriteLine(result6);
+double result7 = Math.Cosh(1);
+Console.WriteLine(result7);
+int result8;
+int div = Math.DivRem(14, 5, out result8);
+Console.WriteLine(result8);
+double result9 = Math.Exp(7.85);
+Console.WriteLine(result9);
+double result10 = Math.Floor(23.6);
+Console.WriteLine(result10);
+double result11 = Math.IEEERemainder(26, 4); // 2 = 26-24
+Console.WriteLine(result11);
+double result12 = Math.Log(30);
+Console.WriteLine(result12);
+double result13 = Math.Log10(80);
+Console.WriteLine(result13);
+double result14 = Math.Log2(78);
+Console.WriteLine(result14);
+double result15 = Math.Max(10, 30);
+Console.WriteLine(result15);
+double result16 = Math.Min(45, 7);
+Console.WriteLine(result16);
+double result17 = Math.Pow(5, 10);
+Console.WriteLine(result17);
+double result18 = Math.Round(8.7);
+Console.WriteLine(result18);
+double result19 = Math.Round(20.567, 2); // 20,57
+Console.WriteLine(result19);
+double result20 = Math.Round(20.463, 1); //20,5
+Console.WriteLine(result20);
+int result21 = Math.Sign(15); // 1
+Console.WriteLine(result21);
+int result22 = Math.Sign(-5); //-1
+Console.WriteLine(result22);
+double result23 = Math.Sin(1);
+Console.WriteLine(result23);
+double result24 = Math.Sinh(0);
+Console.WriteLine(result24);
+double result25 = Math.Sqrt(100);
+Console.WriteLine(result25);
+double result26 = Math.Tan(1);
+Console.WriteLine(result26);
+double result27 = Math.Tanh(0);
+Console.WriteLine(result27);
+double result28 = Math.Truncate(20.4);
+Console.WriteLine(result28);
---
+++ TypeConversionandtheConvertclass/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Globalization;
+
+IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+double number = double.Parse("23.56", formatter);
+Console.WriteLine(number);   // 23,56
+
+int a = int.Parse("10");
+double b = double.Parse("23,56");
+decimal c = decimal.Parse("12,45");
+byte d = byte.Parse("4");
+Console.WriteLine($"a={a}  b={b}  c={c}  d={d}");
+
+Console.WriteLine("Введите строку:");
+string? input = Console.ReadLine();
+
+bool result = int.TryParse(input, out var number1);
+if (result == true)
+    Console.WriteLine($"Преобразование прошло успешно. Число: {number1}");
+else
+    Console.WriteLine("Преобразование завершилось неудачно");
+
+int n = Convert.ToInt32("23");
+bool b1 = true;
+double d1 = Convert.ToDouble(b1);
+Console.WriteLine($"n={n}  d={d1}");
