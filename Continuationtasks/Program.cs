0c90a6748804fd37af5b1705006402f6f63c8993
YuriiKnysh <uriy.knish@gmail.com>
2022-01-20 18:03:27 +0400

    WorkingwithParallel

---
+++ Continuationtasks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(() => {
+                Console.WriteLine($"Id задачи: {Task.CurrentId}");
+            });
+
+            // задача продолжения
+            Task task2 = task1.ContinueWith(Display);
+
+            task1.Start();
+
+            // ждем окончания второй задачи
+            task2.Wait();
+            Console.WriteLine("Выполняется работа метода Main");
+        }
+
+        static void Display(Task t)
+        {
+            Console.WriteLine($"Id задачи: {Task.CurrentId}");
+            Console.WriteLine($"Id предыдущей задачи: {t.Id}");
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Parallel/Program.cs
@@ -0,0 +1,32 @@
+﻿
+{
+    Parallel.Invoke(Display,
+        () => {
+            Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+            Thread.Sleep(3000);
+        },
+        () => Factorial(5));
+
+    Parallel.For(1, 10, Factorial);
+    ParallelLoopResult result = Parallel.ForEach<int>(new List<int>() { 1, 3, 5, 8 },
+                Factorial);
+}
+
+static void Display()
+{
+    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+    Thread.Sleep(3000);
+}
+
+static void Factorial(int x)
+{
+    int result = 1;
+
+    for (int i = 1; i <= x; i++)
+    {
+        result *= i;
+    }
+    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+    Thread.Sleep(3000);
+    Console.WriteLine($"Результат {result}");
+}
