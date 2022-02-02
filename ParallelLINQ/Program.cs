7a0f4dedd4213538d2278a5e167ca57c2f3d3f6a
YuriiKnysh <uriy.knish@gmail.com>
2022-02-02 14:03:10 +0400

    Parallel

---
+++ ParallelLINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, };
+var factorials = from n in numbers.AsParallel()
+                 select Factorial(n);
+foreach (var n in factorials)
+    Console.WriteLine(n);
+Console.ReadLine();
+
+ 
+static int Factorial(int x)
+{
+    int result = 1;
+
+    for (int i = 1; i <= x; i++)
+    {
+        result *= i;
+    }
+    Console.WriteLine($"Факториал числа {x} равен {result}");
+    return result;
+}
---
+++ ParallelOne/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = new int[] { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, };
+var factorials = from n in numbers.AsParallel()
+                     where n > 0
+                     select Factorial(n);
+ foreach (var n in factorials)
+ Console.WriteLine(n);
+
+ Console.Read();
+
+static int Factorial(int x)
+{ 
+    int result = 1;
+
+    for (int i = 1; i <= x; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
