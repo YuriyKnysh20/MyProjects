197c904d4764b4f12c65f70a992bdc8fc40f4522
YuriiKnysh <uriy.knish@gmail.com>
2022-02-26 12:36:22 +0400

    WorkingwithPattern

---
+++ Patterns/Program.cs
@@ -0,0 +1,32 @@
+﻿
+class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+class Manager : User
+{
+    public string Company { get; set; }
+}
+
+public interface IMovable
+{
+    void Move();
+}
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Машина едет");
+    }
+}
+
+class Team
+{
+
+}
+class Player
+{
+    public Team Team { get; set; }
+}
---
+++ XML/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+
+XmlDocument xDoc = new XmlDocument();
+xDoc.Load("D:\\users.xml");
+// получим корневой элемент
+XmlElement? xRoot = xDoc.DocumentElement;
+if (xRoot != null)
+{
+    // обход всех узлов в корневом элементе
+    foreach (XmlElement xnode in xRoot)
+    {
+        // получаем атрибут name
+        XmlNode? attr = xnode.Attributes.GetNamedItem("name");
+        Console.WriteLine(attr?.Value);
+
+        // обходим все дочерние узлы элемента user
+        foreach (XmlNode childnode in xnode.ChildNodes)
+        {
+            // если узел - company
+            if (childnode.Name == "company")
+            {
+                Console.WriteLine($"Компания: {childnode.InnerText}");
+            }
+            // если узел age
+            if (childnode.Name == "age")
+            {
+                Console.WriteLine($"Возраст: {childnode.InnerText}");
+            }
+        }
+        Console.WriteLine();
+    }
+}
